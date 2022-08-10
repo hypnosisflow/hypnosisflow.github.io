fcf626b4a739ba32cc4952c23fb93231a4d1dff5
Artem <plzv92@gmail.com>
2022-08-11 03:10:03 +0700

    port-1.2

---
+++ js/script.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded", function(e) {
+    console.log("DOM fully loaded and parsed");
+
+    const mainComponent = document.getElementById('page-wrap');
+    const lottie = document.getElementById('lottie-pre');
+    const header = document.getElementById('header');
+
+    function contentLoad() {
+        setTimeout(() => {
+            header.classList.add("active");
+        }, 1000);
+        setTimeout(() => {
+            mainComponent.classList.add("active");
+        }, 2500); 
+        setTimeout(() => {
+            lottie.classList.remove("active");
+            lottie.classList.add("fade-out");
+        }, 1250);
+    }
+    contentLoad();
+
+    
+  });
